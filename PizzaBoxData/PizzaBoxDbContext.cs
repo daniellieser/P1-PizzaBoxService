1960e458725181c8394f325fcea38fcea0fb7d19
Daniel Lieser <81538182+daniellieser@users.noreply.github.com>
2021-05-20 13:32:33 -0500

    Update PizzaBoxDbContext.cs

--- PizzaBoxData/PizzaBoxDbContext.cs
+++ PizzaBoxData/PizzaBoxDbContext.cs
@@ -26,7 +26,7 @@ namespace PizzaBoxData
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:pizzaboxlieser.database.windows.net,1433;Initial Catalog=PizzaBoxDb;User ID=dev;Password=Password123");
+                optionsBuilder.UseSqlServer("Server=tcp:***.database.windows.net,1433;Initial Catalog=PizzaBoxDb;User ID=dev;Password=***");
             }
         }
 
